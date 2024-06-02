0686b68b49cf8969411f29c922b57ba3196d72d0
Anton Ivanov <anton.i@hey.com>
2024-06-02 19:50:38 +0300

    last hcnages

---
+++ dotnet-winforms-examples/DatabaseManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Npgsql;
+
+namespace dotnet_winforms_examples
+{
+    internal class DatabaseManager
+    {
+        private static readonly Lazy<DatabaseManager> instance = new Lazy<DatabaseManager>(() => new DatabaseManager());
+
+        private NpgsqlConnection connection;
+
+        private DatabaseManager()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            // Initialize database connection
+            
+            connection = new NpgsqlConnection(connectionString);
+        }
+
+        public static DatabaseManager Instance => instance.Value;
+
+        public NpgsqlConnection GetConnection()
+        {
+            return connection;
+        }
+    }
+}
