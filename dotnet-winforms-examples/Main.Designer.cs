4a6dbe67f972ef191bcdf5907cbcf3c46d76cd7e
Ivanov-Anton <anton.i@hey.com>
2024-06-04 16:16:29 +0300

    Patch2 (#16)
    * fix issue when click to change payment status
    
    * refactoring

--- dotnet-winforms-examples/Main.Designer.cs
+++ dotnet-winforms-examples/Main.Designer.cs
@@ -33,17 +33,24 @@
             tabPage1 = new System.Windows.Forms.TabPage();
             flashMessageLabel = new System.Windows.Forms.Label();
             groupBox1 = new System.Windows.Forms.GroupBox();
+            updateButton2 = new System.Windows.Forms.Button();
             addStudentButton = new System.Windows.Forms.Button();
             listView1 = new System.Windows.Forms.ListView();
+            exitBtn = new System.Windows.Forms.Button();
             tabPage2 = new System.Windows.Forms.TabPage();
+            flashMessageLabelRooms = new System.Windows.Forms.Label();
+            exitBtn2 = new System.Windows.Forms.Button();
             groupBoxOfRooms = new System.Windows.Forms.GroupBox();
             addNewRoomButton = new System.Windows.Forms.Button();
             dataGridViewRooms = new System.Windows.Forms.DataGridView();
             tabPage3 = new System.Windows.Forms.TabPage();
+            flashMessagePaymentsLabel = new System.Windows.Forms.Label();
+            exitBtn3 = new System.Windows.Forms.Button();
             groupBox2 = new System.Windows.Forms.GroupBox();
             updateButton = new System.Windows.Forms.Button();
             dataGridViewPaidPayments = new System.Windows.Forms.DataGridView();
             toolTipForFleshMessage = new System.Windows.Forms.ToolTip(components);
+            updateButton3 = new System.Windows.Forms.Button();
             tabControlRooms.SuspendLayout();
             tabPage1.SuspendLayout();
             groupBox1.SuspendLayout();
@@ -71,17 +78,18 @@
             tabControlRooms.Padding = new System.Drawing.Point(15, 3);
             tabControlRooms.RightToLeft = System.Windows.Forms.RightToLeft.No;
             tabControlRooms.SelectedIndex = 0;
-            tabControlRooms.Size = new System.Drawing.Size(1922, 1101);
+            tabControlRooms.Size = new System.Drawing.Size(1941, 1055);
             tabControlRooms.TabIndex = 0;
             // 
             // tabPage1
             // 
             tabPage1.Controls.Add(flashMessageLabel);
             tabPage1.Controls.Add(groupBox1);
+            tabPage1.Controls.Add(exitBtn);
             tabPage1.Location = new System.Drawing.Point(74, 4);
             tabPage1.Name = "tabPage1";
             tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            tabPage1.Size = new System.Drawing.Size(1844, 1093);
+            tabPage1.Size = new System.Drawing.Size(1863, 1047);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "Студенти";
             tabPage1.UseVisualStyleBackColor = true;
@@ -98,20 +106,31 @@
             // 
             // groupBox1
             // 
+            groupBox1.Controls.Add(updateButton2);
             groupBox1.Controls.Add(addStudentButton);
             groupBox1.Controls.Add(listView1);
-            groupBox1.Location = new System.Drawing.Point(28, 57);
+            groupBox1.Location = new System.Drawing.Point(28, 51);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new System.Drawing.Size(1771, 943);
+            groupBox1.Size = new System.Drawing.Size(1771, 896);
             groupBox1.TabIndex = 1;
             groupBox1.TabStop = false;
-            groupBox1.Text = "Список студентів";
+            groupBox1.Text = "Список студентів.";
+            // 
+            // updateButton2
+            // 
+            updateButton2.Location = new System.Drawing.Point(1212, 115);
+            updateButton2.Name = "updateButton2";
+            updateButton2.Size = new System.Drawing.Size(400, 46);
+            updateButton2.TabIndex = 2;
+            updateButton2.Text = "Оновитии";
+            updateButton2.UseVisualStyleBackColor = true;
+            updateButton2.Click += updateButton2_Click;
             // 
             // addStudentButton
             // 
             addStudentButton.Location = new System.Drawing.Point(1212, 47);
             addStudentButton.Name = "addStudentButton";
-            addStudentButton.Size = new System.Drawing.Size(293, 46);
+            addStudentButton.Size = new System.Drawing.Size(400, 46);
             addStudentButton.TabIndex = 1;
             addStudentButton.Text = "Додати студента";
             addStudentButton.UseVisualStyleBackColor = true;
@@ -121,28 +140,64 @@
             // 
             listView1.Location = new System.Drawing.Point(30, 47);
             listView1.Name = "listView1";
-            listView1.Size = new System.Drawing.Size(1152, 862);
+            listView1.Size = new System.Drawing.Size(1152, 821);
             listView1.TabIndex = 0;
             listView1.UseCompatibleStateImageBehavior = false;
             // 
+            // exitBtn
+            // 
+            exitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            exitBtn.ForeColor = System.Drawing.Color.Red;
+            exitBtn.Location = new System.Drawing.Point(1572, 973);
+            exitBtn.Name = "exitBtn";
+            exitBtn.Size = new System.Drawing.Size(227, 46);
+            exitBtn.TabIndex = 3;
+            exitBtn.Text = "Закрити програму";
+            exitBtn.UseVisualStyleBackColor = true;
+            exitBtn.Click += exitBtn_Click;
+            // 
             // tabPage2
             // 
+            tabPage2.Controls.Add(flashMessageLabelRooms);
+            tabPage2.Controls.Add(exitBtn2);
             tabPage2.Controls.Add(groupBoxOfRooms);
             tabPage2.Location = new System.Drawing.Point(74, 4);
             tabPage2.Name = "tabPage2";
             tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            tabPage2.Size = new System.Drawing.Size(1844, 1093);
+            tabPage2.Size = new System.Drawing.Size(1863, 1047);
             tabPage2.TabIndex = 1;
             tabPage2.Text = "Кімнати";
             tabPage2.UseVisualStyleBackColor = true;
             // 
+            // flashMessageLabelRooms
+            // 
+            flashMessageLabelRooms.ForeColor = System.Drawing.Color.Green;
+            flashMessageLabelRooms.Location = new System.Drawing.Point(30, 19);
+            flashMessageLabelRooms.Name = "flashMessageLabelRooms";
+            flashMessageLabelRooms.Size = new System.Drawing.Size(1800, 45);
+            flashMessageLabelRooms.TabIndex = 5;
+            flashMessageLabelRooms.Click += flashMessageLabelRooms_Click;
+            // 
+            // exitBtn2
+            // 
+            exitBtn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            exitBtn2.ForeColor = System.Drawing.Color.Red;
+            exitBtn2.Location = new System.Drawing.Point(1609, 980);
+            exitBtn2.Name = "exitBtn2";
+            exitBtn2.Size = new System.Drawing.Size(227, 46);
+            exitBtn2.TabIndex = 4;
+            exitBtn2.Text = "Закрити програму";
+            exitBtn2.UseVisualStyleBackColor = true;
+            exitBtn2.Click += exitBtn2_Click;
+            // 
             // groupBoxOfRooms
             // 
+            groupBoxOfRooms.Controls.Add(updateButton3);
             groupBoxOfRooms.Controls.Add(addNewRoomButton);
             groupBoxOfRooms.Controls.Add(dataGridViewRooms);
-            groupBoxOfRooms.Location = new System.Drawing.Point(24, 92);
+            groupBoxOfRooms.Location = new System.Drawing.Point(24, 55);
             groupBoxOfRooms.Name = "groupBoxOfRooms";
-            groupBoxOfRooms.Size = new System.Drawing.Size(1812, 1026);
+            groupBoxOfRooms.Size = new System.Drawing.Size(1812, 902);
             groupBoxOfRooms.TabIndex = 0;
             groupBoxOfRooms.TabStop = false;
             groupBoxOfRooms.Text = "Інформація кімнат гуртожитку";
@@ -164,40 +219,62 @@
             dataGridViewRooms.Name = "dataGridViewRooms";
             dataGridViewRooms.RowHeadersWidth = 82;
             dataGridViewRooms.RowTemplate.Height = 41;
-            dataGridViewRooms.Size = new System.Drawing.Size(1548, 895);
+            dataGridViewRooms.Size = new System.Drawing.Size(1548, 822);
             dataGridViewRooms.TabIndex = 1;
             // 
             // tabPage3
             // 
+            tabPage3.Controls.Add(flashMessagePaymentsLabel);
+            tabPage3.Controls.Add(exitBtn3);
             tabPage3.Controls.Add(groupBox2);
             tabPage3.Location = new System.Drawing.Point(74, 4);
             tabPage3.Name = "tabPage3";
             tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            tabPage3.Size = new System.Drawing.Size(1844, 1093);
+            tabPage3.Size = new System.Drawing.Size(1863, 1047);
             tabPage3.TabIndex = 2;
             tabPage3.Text = "Помісячні Сплати";
             tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // flashMessagePaymentsLabel
+            // 
+            flashMessagePaymentsLabel.ForeColor = System.Drawing.Color.Green;
+            flashMessagePaymentsLabel.Location = new System.Drawing.Point(29, 20);
+            flashMessagePaymentsLabel.Name = "flashMessagePaymentsLabel";
+            flashMessagePaymentsLabel.Size = new System.Drawing.Size(1783, 42);
+            flashMessagePaymentsLabel.TabIndex = 6;
+            flashMessagePaymentsLabel.Click += flashMessagePaymentsLabel_Click;
+            // 
+            // exitBtn3
+            // 
+            exitBtn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            exitBtn3.ForeColor = System.Drawing.Color.Red;
+            exitBtn3.Location = new System.Drawing.Point(1585, 988);
+            exitBtn3.Name = "exitBtn3";
+            exitBtn3.Size = new System.Drawing.Size(227, 46);
+            exitBtn3.TabIndex = 5;
+            exitBtn3.Text = "Закрити програму";
+            exitBtn3.UseVisualStyleBackColor = true;
+            exitBtn3.Click += exitBtn3_Click;
             // 
             // groupBox2
             // 
             groupBox2.Controls.Add(updateButton);
             groupBox2.Controls.Add(dataGridViewPaidPayments);
-            groupBox2.Location = new System.Drawing.Point(20, 85);
+            groupBox2.Location = new System.Drawing.Point(20, 56);
             groupBox2.Name = "groupBox2";
-            groupBox2.Size = new System.Drawing.Size(1792, 1035);
+            groupBox2.Size = new System.Drawing.Size(1792, 938);
             groupBox2.TabIndex = 0;
             groupBox2.TabStop = false;
             groupBox2.Text = "Помісячні сплати за житло в гуртожитку";
             // 
             // updateButton
             // 
-            updateButton.BackgroundImage = Properties.Resources.refresh_icon1;
             updateButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             updateButton.Location = new System.Drawing.Point(1432, 49);
             updateButton.Name = "updateButton";
             updateButton.Size = new System.Drawing.Size(278, 81);
             updateButton.TabIndex = 1;
-            updateButton.Text = "     Оновити";
+            updateButton.Text = "Оновити";
             updateButton.UseVisualStyleBackColor = true;
             updateButton.Click += updateButton_Click;
             // 
@@ -206,16 +283,28 @@
             dataGridViewPaidPayments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridViewPaidPayments.Location = new System.Drawing.Point(23, 49);
             dataGridViewPaidPayments.Name = "dataGridViewPaidPayments";
+            dataGridViewPaidPayments.ReadOnly = true;
             dataGridViewPaidPayments.RowHeadersWidth = 82;
             dataGridViewPaidPayments.RowTemplate.Height = 41;
-            dataGridViewPaidPayments.Size = new System.Drawing.Size(1382, 956);
+            dataGridViewPaidPayments.Size = new System.Drawing.Size(1382, 868);
             dataGridViewPaidPayments.TabIndex = 0;
+            // 
+            // updateButton3
+            // 
+            updateButton3.Location = new System.Drawing.Point(1591, 112);
+            updateButton3.Name = "updateButton3";
+            updateButton3.Size = new System.Drawing.Size(196, 46);
+            updateButton3.TabIndex = 3;
+            updateButton3.Text = "Оновити";
+            updateButton3.UseVisualStyleBackColor = true;
+            updateButton3.Click += updateButton3_Click;
             // 
             // Main
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(1922, 1101);
+            ClientSize = new System.Drawing.Size(1941, 1055);
+            ControlBox = false;
             Controls.Add(tabControlRooms);
             Name = "Main";
             Text = "Main";
@@ -249,5 +338,12 @@
         private System.Windows.Forms.Button updateButton;
         private System.Windows.Forms.Label flashMessageLabel;
         private System.Windows.Forms.ToolTip toolTipForFleshMessage;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Button exitBtn2;
+        private System.Windows.Forms.Button exitBtn3;
+        private System.Windows.Forms.Label flashMessageLabelRooms;
+        private System.Windows.Forms.Label flashMessagePaymentsLabel;
+        private System.Windows.Forms.Button updateButton2;
+        private System.Windows.Forms.Button updateButton3;
     }
 }
--- dotnet-winforms-examples/Main.cs
+++ dotnet-winforms-examples/Main.cs
@@ -24,13 +24,18 @@ namespace dotnet_winforms_examples
             if (flashMessage.Length > 0)
             {
                 flashMessageLabel.Text = flashMessage;
+                flashMessageLabelRooms.Text = flashMessage;
+                flashMessagePaymentsLabel.Text = flashMessage;
                 // Set up the delays for the ToolTip.
                 toolTipForFleshMessage.AutoPopDelay = 0;
                 toolTipForFleshMessage.InitialDelay = 1000;
                 toolTipForFleshMessage.ReshowDelay = 500;
                 flashMessageLabel.MouseEnter += flashMessagelabelMouseEnterEvent;
                 flashMessageLabel.MouseLeave += flashMessagelabelMouseLeaveEvent;
-                toolTipForFleshMessage.SetToolTip(flashMessageLabel, "Клацніть для того щоб приховати ваше сповіщення");
+                string tollTipMessage = "Клацніть для того щоб приховати ваше повідомлення";
+                toolTipForFleshMessage.SetToolTip(flashMessageLabel, tollTipMessage);
+                toolTipForFleshMessage.SetToolTip(flashMessageLabelRooms, tollTipMessage);
+                toolTipForFleshMessage.SetToolTip(flashMessagePaymentsLabel, tollTipMessage);
             }
         }
 
@@ -47,6 +52,69 @@ namespace dotnet_winforms_examples
         }
 
         private void Main_Load(object sender, EventArgs e)
+        {
+            LoadStudents();
+
+            // rooms
+
+            LoadRoomsData();
+
+            // Add columns
+            dataGridViewRooms.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "id", DataPropertyName = "id", Width = 70 });
+            dataGridViewRooms.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Номер етажа", DataPropertyName = "floor", Width = 100 });
+            dataGridViewRooms.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Номер кімнати", DataPropertyName = "number", Width = 100 });
+            dataGridViewRooms.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Доступно місць", DataPropertyName = "available_places", Width = 110 });
+            dataGridViewRooms.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Всього місць", DataPropertyName = "places", Width = 100 });
+            dataGridViewRooms.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Комфорт", DataPropertyName = "comfort", Width = 120 });
+            dataGridViewRooms.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Балкон", DataPropertyName = "has_balcony", Width = 120 });
+            dataGridViewRooms.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Холодильник", DataPropertyName = "has_fridge", Width = 120 });
+            dataGridViewRooms.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Мікрохвилоьва пічь", DataPropertyName = "has_microwave", Width = 150 });
+            dataGridViewRooms.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Ціна", DataPropertyName = "price", Width = 120 });
+            // Add action delete button column
+            var actionRemoveButtonColumn = new DataGridViewButtonColumn
+            {
+                HeaderText = "Управління",
+                Text = "Видалити",
+                UseColumnTextForButtonValue = true
+            };
+            dataGridViewRooms.Columns.Add(actionRemoveButtonColumn);
+            dataGridViewRooms.CellClick += dataGridViewRooms_RemoveCellClick;
+
+            var addStudentColumnAction = new DataGridViewButtonColumn
+            {
+                HeaderText = "Управління",
+                Text = "Додати студента",
+                UseColumnTextForButtonValue = true
+            };
+            dataGridViewRooms.Columns.Add(addStudentColumnAction);
+            dataGridViewRooms.CellClick += dataGridViewRooms_CellClickToAddStudent;
+
+
+            // payments
+
+            LoadPaidPaymentsData();
+            dataGridViewPaidPayments.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "id", DataPropertyName = "id", Width = 40 });
+            dataGridViewPaidPayments.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "За який місяц платіж", DataPropertyName = "month", Width = 200 });
+            dataGridViewPaidPayments.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Статус", DataPropertyName = "status", Width = 150 });
+            dataGridViewPaidPayments.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Студент", DataPropertyName = "student_full_name", Width = 320 });
+            dataGridViewPaidPayments.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Сума", DataPropertyName = "monthly_amount", Width = 100 });
+            dataGridViewPaidPayments.CellFormatting += dataGridViewPaidPayments_CellFormatting;
+            // Add action button column
+            var actionMarkAsPaidButton = new DataGridViewButtonColumn
+            {
+                HeaderText = "Управління",
+                Text = "Помітити як сплачений",
+                UseColumnTextForButtonValue = true,
+                Width = 300
+            };
+            dataGridViewPaidPayments.Columns.Add(actionMarkAsPaidButton);
+            dataGridViewPaidPayments.CellClick += dataGridViewPaidPayments_CellClick;
+
+            //command.Dispose();
+            connection.Close();
+        }
+
+        private void LoadStudents() 
         {
             connection = DatabaseManager.Instance.GetConnection();
             connection.Open();
@@ -88,64 +156,6 @@ namespace dotnet_winforms_examples
             {
                 listView1.Items.Add(new ListViewItem(new[] { reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4) }));
             }
-
-            // rooms
-
-            LoadRoomsData();
-
-            // Add columns
-            dataGridViewRooms.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "id", DataPropertyName = "id", Width = 40 });
-            dataGridViewRooms.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Номер етажа", DataPropertyName = "floor", Width = 100 });
-            dataGridViewRooms.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Номер кімнати", DataPropertyName = "number", Width = 100 });
-            dataGridViewRooms.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Доступно місць", DataPropertyName = "available_places", Width = 100 });
-            dataGridViewRooms.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Всього місць", DataPropertyName = "places" });
-            dataGridViewRooms.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Комфорт", DataPropertyName = "comfort", Width = 120 });
-            dataGridViewRooms.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Балкон", DataPropertyName = "has_balcony", Width = 120 });
-            dataGridViewRooms.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Холодильник", DataPropertyName = "has_fridge", Width = 120 });
-            dataGridViewRooms.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Мікрохвилоьва пічь", DataPropertyName = "has_microwave", Width = 120 });
-            dataGridViewRooms.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Ціна", DataPropertyName = "price", Width = 120 });
-            // Add action delete button column
-            var actionRemoveButtonColumn = new DataGridViewButtonColumn
-            {
-                HeaderText = "Управління",
-                Text = "Видалити",
-                UseColumnTextForButtonValue = true
-            };
-            dataGridViewRooms.Columns.Add(actionRemoveButtonColumn);
-            dataGridViewRooms.CellClick += dataGridViewRooms_RemoveCellClick;
-
-            var addStudentColumnAction = new DataGridViewButtonColumn
-            {
-                HeaderText = "Управління",
-                Text = "Додати студента",
-                UseColumnTextForButtonValue = true
-            };
-            dataGridViewRooms.Columns.Add(addStudentColumnAction);
-            dataGridViewRooms.CellClick += dataGridViewRooms_CellClickToAddStudent;
-
-
-            // payments
-
-            LoadPaidPaymentsData();
-            dataGridViewPaidPayments.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "id", DataPropertyName = "id", Width = 40 });
-            dataGridViewPaidPayments.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "За який місяц платіж", DataPropertyName = "month", Width = 200 });
-            dataGridViewPaidPayments.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Статус", DataPropertyName = "status", Width = 150 });
-            dataGridViewPaidPayments.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Студент", DataPropertyName = "student_full_name", Width = 320 });
-            dataGridViewPaidPayments.Columns.Add(new DataGridViewTextBoxColumn { HeaderText = "Сума", DataPropertyName = "monthly_amount", Width = 100 });
-            dataGridViewPaidPayments.CellFormatting += dataGridViewPaidPayments_CellFormatting;
-            // Add action button column
-            var actionMarkAsPaidButton = new DataGridViewButtonColumn
-            {
-                HeaderText = "Управління",
-                Text = "Помітити я сплачений",
-                UseColumnTextForButtonValue = true,
-                Width = 300
-            };
-            dataGridViewPaidPayments.Columns.Add(actionMarkAsPaidButton);
-            dataGridViewPaidPayments.CellClick += dataGridViewPaidPayments_CellClick;
-
-            //command.Dispose();
-            connection.Close();
         }
 
         private void dataGridViewRooms_RemoveCellClick(object sender, DataGridViewCellEventArgs e)
@@ -164,7 +174,8 @@ namespace dotnet_winforms_examples
             if (e.RowIndex >= 0 && e.ColumnIndex == 11)
             {
                 string id = dataGridViewRooms.Rows[e.RowIndex].Cells[0].Value.ToString();
-                AddStudentToRoomPopUp form = new AddStudentToRoomPopUp(int.Parse(id));
+                string price = dataGridViewRooms.Rows[e.RowIndex].Cells[9].Value.ToString();
+                AddStudentToRoomPopUp form = new AddStudentToRoomPopUp(int.Parse(id), price);
                 this.Hide();
                 form.Show();
             }
@@ -208,7 +219,7 @@ namespace dotnet_winforms_examples
 
         private void dataGridViewPaidPayments_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            if (e.RowIndex >= 0 && e.ColumnIndex == 3)
+            if (e.RowIndex >= 0 && e.ColumnIndex == 5)
             {
                 var id = dataGridViewPaidPayments.Rows[e.RowIndex].Cells[0].Value.ToString();
                 string query = "UPDATE payments SET status = 'Paid' WHERE id = @Id";
@@ -222,10 +233,11 @@ namespace dotnet_winforms_examples
                     connToUpdatePayment.Close();
                 }
 
+                flashMessageLabel.Text = "Статус успішно змінено";
                 // Refresh the DataGridView to reflect the changes
                 dataGridViewPaidPayments.DataSource = null;
                 LoadPaidPaymentsData();
-                flashMessageLabel.Text = "Статус успішно змінено";
+
 
             }
         }
@@ -292,7 +304,59 @@ namespace dotnet_winforms_examples
 
         private void flashMessageLabel_Click(object sender, EventArgs e)
         {
+            flashMessageLabelRooms.Text = "";
+            flashMessagePaymentsLabel.Text = "";
             flashMessageLabel.Text = "";
+        }
+
+        private void closeAllForms()
+        {
+            for (int i = Application.OpenForms.Count - 1; i != -1; i = Application.OpenForms.Count - 1)
+            {
+                Application.OpenForms[i].Close();
+            }
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            closeAllForms();
+        }
+
+        private void exitBtn2_Click(object sender, EventArgs e)
+        {
+            closeAllForms();
+        }
+
+        private void exitBtn3_Click(object sender, EventArgs e)
+        {
+            closeAllForms();
+        }
+
+        private void flashMessageLabelRooms_Click(object sender, EventArgs e)
+        {
+            flashMessageLabelRooms.Text = "";
+            flashMessagePaymentsLabel.Text = "";
+            flashMessageLabel.Text = "";
+        }
+
+        private void flashMessagePaymentsLabel_Click(object sender, EventArgs e)
+        {
+            flashMessagePaymentsLabel.Text = "";
+            flashMessageLabel.Text = "";
+            flashMessageLabelRooms.Text = "";
+        }
+
+        // update from Students page
+        private void updateButton2_Click(object sender, EventArgs e)
+        {
+            LoadStudents();
+        }
+
+    
+        // update from Rooms page
+        private void updateButton3_Click(object sender, EventArgs e)
+        {
+            LoadRoomsData();
         }
     }
 }
